d4773a0b28b16a8d03c25f5d989d3ccedf4b5a2d
sagurosky <danielsagurosky@gmail.com>
2025-08-13 13:55:51 -0300

    configuracion ci/cd

--- src/test/java/mantenimiento/gestorTareas/GestorTareasApplicationTests.java
+++ src/test/java/mantenimiento/gestorTareas/GestorTareasApplicationTests.java
@@ -2,12 +2,15 @@ package mantenimiento.gestorTareas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Esto asegura que use application-test.properties
 class GestorTareasApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Este test verifica que el contexto de Spring se carga correctamente
+		// No necesita código, si llega aquí es que pasó
 	}
-
 }
