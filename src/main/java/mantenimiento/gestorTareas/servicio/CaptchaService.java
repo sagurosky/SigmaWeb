ee7ad10f9559f8f2b1075cf7cd35293de08ccd76
sagurosky <danielsagurosky@gmail.com>
2025-09-09 10:15:11 -0300

    debug

--- src/main/java/mantenimiento/gestorTareas/servicio/CaptchaService.java
+++ src/main/java/mantenimiento/gestorTareas/servicio/CaptchaService.java
@@ -8,27 +8,30 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-
 @Service
 public class CaptchaService {
 
     private static final String GOOGLE_RECAPTCHA_VERIFY_URL =
             "https://www.google.com/recaptcha/api/siteverify";
 
-    private String recaptchaSecret= ArchivoExterno.getString("captcha_secret");
-
+    private String recaptchaSecret = ArchivoExterno.getString("captcha_secret");
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public boolean validarCaptcha(String responseToken) {
-        Map<String, String> body = new HashMap<>();
-        body.put("secret", recaptchaSecret);
-        body.put("response", responseToken);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("secret", recaptchaSecret);
+        params.add("response", responseToken);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request =
+                new HttpEntity<>(params, headers);
 
         ResponseEntity<Map> recaptchaResponse =
-                restTemplate.postForEntity(GOOGLE_RECAPTCHA_VERIFY_URL, body, Map.class);
+                restTemplate.postForEntity(GOOGLE_RECAPTCHA_VERIFY_URL, request, Map.class);
 
-        System.out.println("Secret: "+recaptchaSecret );
         System.out.println("Respuesta CAPTCHA: " + recaptchaResponse.getBody());
 
         if (recaptchaResponse.getBody() == null) return false;
@@ -36,4 +39,3 @@ public class CaptchaService {
         return (Boolean) recaptchaResponse.getBody().get("success");
     }
 }
-
